fe12163d7897d2e74f81164e0649ecfbea60839b
Bhanu Prakash Bhaskarla <123292221+bhanuprakash1606@users.noreply.github.com>
2023-07-04 11:45:20 +0530

    main.js of Example Animation

---
+++ Example/main.js
@@ -0,0 +1,15 @@
+anime({
+    targets: "div.box",
+    translateY: [
+        {value: 200, duration: 500},
+        {value: 0, duration: 800},
+    ],
+    rotate: {
+        value: "1turn",
+        easing: "easeInOutSine"
+    },
+    delay: function (el, i, l) {
+        return i * 1000;
+    },
+    loop: true,
+});
